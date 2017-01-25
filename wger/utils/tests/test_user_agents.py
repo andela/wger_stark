aec7f6981d1589739485785929c0bfa23819f720
kayeli dennis <kdenno25@gmail.com>
2017-01-25 11:08:37 +0300

    code refactor

--- wger/utils/tests/test_user_agents.py
+++ wger/utils/tests/test_user_agents.py
@@ -109,8 +109,10 @@ class AndroidUserAgentTestCase(WorkoutManagerTestCase):
         Tests the old webview user agent
         '''
 
-        agent = "Mozilla/5.0 (Linux; Android 4.4; Nexus 5 Build/BuildID) AppleWebKit/537.36 "\
-                "(KHTML, like Gecko) Version/4.0 Chrome/30.0.0.0 Mobile Safari/537.36 "\
+        agent = "Mozilla/5.0 (Linux; Android 4.4; Nexus 5 Build/BuildID) \
+                AppleWebKit/537.36 "\
+                "(KHTML, like Gecko) Version/4.0 Chrome/30.0.0.0 \
+                Mobile Safari/537.36 "\
                 "WgerAndroidWebApp"
         self.assertTrue(user_agents.is_android_webview(agent))
 
